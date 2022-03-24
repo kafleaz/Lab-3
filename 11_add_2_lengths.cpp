18b0a641144c7b2be11fd28730382013580680bd
kafleaz <rajish126@gmail.com>
2022-03-24 11:51:22 +0545

    cpp

--- 11_add_2_lengths.cpp
+++ 11_add_2_lengths.cpp
@@ -2,11 +2,29 @@
 using namespace std;
 class length
 {
-    int l;
+    float f, inc;
     public:
     void setdata()
     {
-        cout<<"length: ";
-        cin>>l; 
+        cout<<"Enter length in feet: ";
+        cin>>f; 
+        cout<<"Enter length in inches: ";
+        cin>>inc;
+    }
+    float inch()
+    {
+        return(inc/12);
+    }
+    void display()
+    {
+        float sum;
+        sum=f+inch();
+        cout<<"After adding the length in is: "<<sum<<" feet";
     }
 };
+int main()
+{
+    length l1;
+    l1.setdata();
+    l1.display();
+}
