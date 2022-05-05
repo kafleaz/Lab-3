49f7214eb81137f7278e3a9b2371ec8746ae74a4
kafleaz <rajish126@gmail.com>
2022-05-05 23:29:13 +0545

    cpp

--- 10_add_complex.cpp
+++ 10_add_complex.cpp
@@ -6,29 +6,32 @@ class complex
     public:
     void setdata()
     {
-        
+
         cin>>rel>>img;
     }
     int imaginary()
     {
-        return(img+img);
+        return(img);
     }
     int real()
     {
-        return(rel+rel);
+        return(rel);
     }
     void display()
     {
+        int r,i;
+        r=img + img;
+         
         cout<<"The addition of given data is"<<endl << real()<<" + "<<imaginary()<<"i";
     }
 };
 int main()
 {
-    complex c1,c2;
+    complex c1,c2,c3;
     cout<<"Enter the first real and imaginary value."<<endl;
     c1.setdata();
-    cout<<"Enter the second real and imaginary value."<<endl;
-    c2.setdata();
+    //cout<<"Enter the second real and imaginary value."<<endl;
+    //c2.setdata();
     c1.display();
     
 }
